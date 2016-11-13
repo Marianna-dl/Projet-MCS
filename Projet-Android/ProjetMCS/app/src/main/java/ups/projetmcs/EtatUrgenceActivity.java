36a0a8284d056f9e6fb78628f957a896fd510b62
LeaFoissac <lea.foissac@gmail.com>
2016-11-13 15:05:59 +0100

    Ajout gif minion + son "beedo beedo" pour l'état d'urgence

--- Projet-Android/ProjetMCS/app/src/main/java/ups/projetmcs/EtatUrgenceActivity.java
+++ Projet-Android/ProjetMCS/app/src/main/java/ups/projetmcs/EtatUrgenceActivity.java
@@ -1,13 +1,26 @@
 package ups.projetmcs;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class EtatUrgenceActivity extends Activity {
+
+    private MediaPlayer mPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.etat_urgence_layout);
+        playSound(R.raw.bee_do);
+    }
+
+    private void playSound(int resId) {
+        if(mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+        }
+        mPlayer = MediaPlayer.create(this, resId);
+        mPlayer.start();
     }
 }
